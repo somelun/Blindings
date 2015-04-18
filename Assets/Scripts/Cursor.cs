526c5ae6bc4d74355be6228178adefc569eff028
some_lun <info@lunatic.su>
2015-04-18 12:57:24 +0600

    Add cursor

---
+++ Assets/Scripts/Cursor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cursor : MonoBehaviour {
+
+	public Texture2D cursorImage;
+
+	private void Start() {
+    	UnityEngine.Cursor.visible = false;
+	}
+	
+	private void OnGUI() {
+    	Vector3 mousePosition = Input.mousePosition;
+    	GUI.DrawTexture(new Rect(mousePosition.x, Screen.height - mousePosition.y, 2, 2), cursorImage);
+	}
+}
