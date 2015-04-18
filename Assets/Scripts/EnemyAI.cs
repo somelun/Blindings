1cf6e18672a0e7b6ee8713d8df52f5dd832731fc
some_lun <info@lunatic.su>
2015-04-18 20:26:26 +0600

    Fix player detection by ai, again

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -13,12 +13,7 @@ public class EnemyAI : MonoBehaviour {
 	}
 	
 	private void Update() {
-		transform.LookAt(_player.transform);
 		if (Physics.Raycast(transform.position, _player.transform.position - transform.position, out _raycastHit, _viewDistance)) {
-
-			Debug.DrawLine(transform.position, _player.transform.position, Color.red);
-			Debug.Log(_raycastHit.collider.gameObject);
-
 			if (_raycastHit.collider.gameObject == _player) {
 				Debug.Log("I see you!");
 			}
